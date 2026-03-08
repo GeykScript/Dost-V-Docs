72e93b97a0a1a06ad3e56ed9be5b4692f00ff99f
GeykScript <JeikuDevs@gmail.com>
2026-03-08 16:49:59 +0800

    Initial Commit: Create Login Page Design

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,67 @@
+<x-guest-layout>
+
+    <div class="flex flex-col px-6 md:px-12">
+        <div class="flex justify-center items-center mt-10">
+            <div class="flex">
+                <img src="{{ asset ('logo/docs_logov2.png')}}" alt="DOCS Logo" class="w-auto h-16 md:h-20">
+                <div class="flex flex-col justify-center ">
+                    <h1 class="text-2xl text-brand-blue font-black italic leading-tight tracking-tight"> <span class="text-brand-dark-blue">D</span>OCS</h1>
+                    <h3 class="font-semibold text-gray-800 text-[9px] md:text-sm"><span class="text-brand-dark-blue font-semibold">DOCUMENT</span> OPERATION COMMUNICATION SYSTEM</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex justify-center items-center my-6">
+            <h1 class="font-semibold  text-brand-blue text-md md:text-lg" >Login Account</h1>
+        </div>
+
+            <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}" >
+        @csrf
+        <!-- Email Address -->
+        <div>
+            <!-- NEED TO CHANGE ACCOUNT ID -->
+            <x-input-label for="email" :value="__('Account ID')" class="text-gray-800" />
+            <x-text-input id="email" class="block mt-1 w-full h-10 md:h-12 " type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full h-10 md:h-12"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <!-- <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div> -->
+
+        <div class="flex mt-6">
+            <!-- @if (Route::has('password.request'))
+            
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif -->
+
+            <x-primary-button class="w-full text-center flex justify-center mb-12 h-10 md:h-12 items-center bg-gray-800">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+    </div>
+
+
+</x-guest-layout>
---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
+        <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans text-gray-800 antialiased">
+        <div class="flex flex-col  p-4 bg-white gap-3">
+            <!-- Dost Header  -->
+            <div class="flex gap-2 p-3 md:px-20">   
+                <img src="{{ asset('logo/dost_logo.svg') }}" alt="DOST Logo" class="w-auto h-9 md:h-12">
+                <div class="flex flex-col text-gray-800">
+                    <ul class="m-0 p-0">
+                        <li class="leading-none m-0 font-bold text-[12px] md:text-[20px]">DOST</li>
+                        <li class="leading-none m-0 text-[10px] md:text-[14px]">BICOL</li>
+                        <li class="leading-none m-0 font-semibold text-[8px] md:text-[12px] truncate">OneDOST4U: Solutions and Opportunities</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Login -->
+            <div class="flex items-center justify-center w-full">
+                <div class="w-full sm:max-w-xl bg-white shadow-lg sm:rounded-lg">
+                    <img src="{{ asset ('logo/dost_banner.png') }}" alt="DOST Banner" class="shadow-md">
+                    {{ $slot }}
+                </div>
+            </div>
+        </div>
+        <!-- footer part -->
+        <footer class="my-3 ">
+            <div class="text-[8px] md:text-[10px] flex flex-col items-center justify-center">
+                <p>© 2026 All rights reserved </p>
+                <p>Developed by Department of Science and Technology - Regional Office V</p>
+                <p>Management Information Services Unit</p>
+            </div>
+        </footer>
+    </body>
+</html>
