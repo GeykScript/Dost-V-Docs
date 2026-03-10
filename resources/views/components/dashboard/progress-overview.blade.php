f6b2d865918c2d457d730ed4b71bc202fbce0193
GeykScript <JeikuDevs@gmail.com>
2026-03-10 14:24:11 +0800

    Fix: Graph tooltip

--- resources/views/components/dashboard/progress-overview.blade.php
+++ resources/views/components/dashboard/progress-overview.blade.php
@@ -1,29 +1,28 @@
-<div class="col-span-1 lg:col-span-2 bg-white rounded-lg p-6">
-    <h1 class="text-gray-700 font-semibold text-lg">Progress Overview</h1>
-    <hr class="border-b border-gray-200 mt-4 w-full">
-    <div class="w-full">
-        <div class="w-full border-b border-gray-200">
-            <div class="flex items-start justify-between p-4 w-full">
-                <div class="w-full">
-                    <div class="flex justify-between">
-                        <h3 class="text-md font-semibold text-gray-700">For Signature<h3>
-                            <div class="px-3 py-1 bg-sky-100/70 rounded-full">
-                        <p class="text-brand-blue text-[10px]">Routed</p>
-                    </div>
-                    </div>
-                    
-                    <p class="text-xs font-medium text-gray-600">Memorandum of Agreement with Bicol Uni...</p>
-                    <p class="text-xs font-medium italic text-blue-600">For Signature</p>
-                    <div class="flex justify-between items-end">
-                         <div class=" mt-2 w-full flex flex-col">
-                            <p class="text-xs font-medium text-gray-600">Source: MIS - Juan De La Cruz</p>
-                            <p class="text-xs font-medium text-gray-600">Routed to: Giannis Antetokounmpo</p>
-
+    <div class="col-span-1 lg:col-span-2 bg-white rounded-lg p-6">
+        <h1 class="text-gray-700 font-semibold text-lg">Progress Overview</h1>
+        <hr class="border-b border-gray-200 mt-4 w-full">
+        <div class="w-full">
+            <div class="w-full border-b border-gray-200">
+                <div class="flex items-start justify-between py-3 w-full">
+                    <div class="w-full">
+                        <div class="flex justify-between mb-1">
+                            <h3 class="text-md font-semibold text-gray-700">For Signature<h3>
+                                <div class="px-3 py-1 bg-sky-100/70 rounded-full">
+                                    <p class="text-brand-blue text-[10px]">Routed</p>
+                                </div>
                         </div>
-                        <p class="font-medium italic text-xs text-gray-600 max-w-16 w-full text-end">5 mins</p>
+                        
+                            <p class="text-xs font-medium text-gray-600">Memorandum of Agreement with Bicol Uni...</p>
+                            <p class="text-xs font-medium italic text-blue-600">For Signature</p>
+                        <div class="flex justify-between items-end">
+                            <div class=" mt-2 w-full flex flex-col">
+                                <p class="text-xs font-medium text-gray-600">Source: MIS - Juan De La Cruz</p>
+                                <p class="text-xs font-medium text-gray-600">Routed to: Giannis Antetokounmpo</p>
+                            </div>
+                                <p class="font-medium italic text-xs text-gray-600 max-w-16 w-full text-end">5 mins</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
