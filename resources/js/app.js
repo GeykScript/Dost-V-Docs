60c358ec9b1775e77ccd490d0fde0c4fd0ca37d8
John Ivan Belaro <belaroivan17@gmail.com>
2026-03-10 08:39:10 +0800

    Add: Initial Sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,25 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import sideMenu from './components/side-menu.js';
 import './datetime';
+
+
+Alpine.store('sidebar', {
+    open: localStorage.getItem('sidebarOpen') === 'true', // Defaults to false if not set
+
+    toggle() {
+        this.open = !this.open;
+        localStorage.setItem('sidebarOpen', this.open);
+    },
+
+    close() {
+        this.open = false;
+        localStorage.setItem('sidebarOpen', 'false');
+    }
+});
+
+Alpine.data('sideMenu', sideMenu);
 
 window.Alpine = Alpine;
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,26 +12,22 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <!-- Livewire Styles -->
         @livewireStyles
-    </head>
+    </head>    
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        <div class="min-h-screen bg-gray-100 h-screen flex">
+            
+            @include('components.sidebar')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+            <div class="flex flex-col flex-1 overflow-hidden">
+                
+                @include('layouts.navigation')
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <main class="flex-1 overflow-y-auto bg-gray-100">
+                    {{ $slot }}
+                </main>
+            </div>
+            
         </div>
-        <!-- Livewire Scripts -->
         @livewireScripts
     </body>
 </html>
